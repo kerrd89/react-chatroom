25db869cdc9c22938c72c3f86a1806dbc39c74e3
kylem038 <misenckj@gmail.com>
2016-09-21 15:59:34 -0600

    Keepin on testin

---
+++ test/mockmessages.js
@@ -0,0 +1,115 @@
+const mockMessages = [
+    {
+        key: "-KS3GtKnWB_XZKjgb0iJ",
+        content: "Scrbabble",
+        createdAt: 1474319324550,
+        user: {
+            displayName: "Casey Cross",
+            email: "hellocaseycross@gmail.com",
+            uid: "oSMIlUTN5OZOVY9pYfEi1PXCv9q1"
+        }
+    },
+    {
+        key: "-KS3ONDVgCurMmG_dP4-",
+        content: "testing out a new message",
+        createdAt: 1474321285995,
+        user: {
+            displayName: "Jeff Duke",
+            email: "jeff.duke@gmail.com",
+            uid: "8opUzbSXdxOJcKgukMF4oIMPTu12"
+        }
+    },
+    {
+        key: "-KS3PZUG1tUAqLLmHOpE",
+        content: "test message 123",
+        createdAt: 1474321598356,
+        user: {
+            displayName: "Jeff Duke",
+            email: "jeff.duke@gmail.com",
+            uid: "8opUzbSXdxOJcKgukMF4oIMPTu12"
+        }
+    },
+    {
+        key: "-KS2zFc3pkfHdvQ14NPp",
+        content: "still works i think",
+        createdAt: 1474314439371,
+        user: {
+            displayName: "Andy Young",
+            email: "atyoungdesign@gmail.com",
+            uid: "aL0MeDz7oXRqtSBPRYguthVf5aw2"
+        }
+    },
+    {
+        key: "-KSA2O4QroqvwYLgBEyX",
+        content: "styling what what",
+        createdAt: 1474432962911,
+        user: {
+            displayName: "Kris Foss",
+            email: "kris.foss@gmail.com",
+            uid: "U59NDKN6qsTbHYXLJdGqByX54BE2"
+        }
+    },
+    {
+        key: "-KSC5YSPJe_9Q9lFU-jH",
+        content: "Does this work?",
+        createdAt: 1474467346610,
+        user: {
+            displayName: "Brett Hevia",
+            email: "brett.hevia@gmail.com",
+            uid: "vOaZV0E03SRRyGnpE8XMm6koZZA3"
+        }
+    },
+    {
+        key: "-KRz_rmSlxBeifO0G3oK",
+        content: "setting TEXT to two",
+        createdAt: 1474240682626,
+        user: {
+            displayName: "Madison Kerndt",
+            email: "madison.kerndt@gmail.com",
+            uid: "eDnoVr1f5OZlySEszpbYSI2IcxG2"
+
+        }
+    },
+    {
+        key: '-KSAMLo5iSmClrZSPFXv',
+        content: '1',
+        createdAt: 1474438196301,
+        user: {
+            displayName: 'Madison Kerndt',
+            email: 'madison.kerndt@gmail.com',
+            uid: 'eDnoVr1f5OZlySEszpbYSI2IcxG2'
+        }
+    },
+    {
+        key: '-KS9Mid4HGVWZxqxn4OQ',
+        content: 'hello',
+        createdAt: 1474421516861,
+        user: {
+            displayName: 'Madison Kerndt',
+            email: 'madison.kerndt@gmail.com',
+            uid: 'eDnoVr1f5OZlySEszpbYSI2IcxG2'
+        }
+    },
+    {
+        key: '-KSCZ5iGbfUDQY5YmVwg',
+        content: 'hello',
+        createdAt: 1474475092826,
+        user: {
+            displayName: 'Adam Bernard',
+            email: 'ab255@me.com',
+            uid: 'WtVvQ5Sk1vOaAU5vaG8d6FolvZb2'
+        }
+    },
+  {
+        key: '-KSC8Xw5nmc8seU9ywKf',
+        content: 'say what',
+        createdAt: 1474468130583,
+        user: {
+            displayName: 'Adam Bernard',
+            email: 'ab255@me.com',
+            uid: 'WtVvQ5Sk1vOaAU5vaG8d6FolvZb2'
+        }
+    }
+];
+
+module.exports = mockMessages;
